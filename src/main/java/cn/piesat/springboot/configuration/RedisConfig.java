e7d3d2b9c61452c53ce3e8512b952014479aef6d
506683059@qq.com <506683059@qq.com>
2018-09-11 16:56:41 +0800

    优化redis配置，可以直接存储对象，下一步封装

--- src/main/java/cn/piesat/springboot/configuration/RedisConfig.java
+++ src/main/java/cn/piesat/springboot/configuration/RedisConfig.java
@@ -1,5 +1,8 @@
 package cn.piesat.springboot.configuration;
 
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonRedisSerializer;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
@@ -9,7 +12,10 @@ import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.io.Serializable;
 
 /**
  * @author zk
@@ -24,16 +30,27 @@ public class RedisConfig {
     }
 
     @Bean
-    public RedisTemplate<String,String> redisTemplate(){
-        RedisTemplate<String,String> redisTemplate = new StringRedisTemplate();
+    public RedisTemplate<String,Object> redisTemplate(){
+        RedisTemplate<String,Object> redisTemplate = new RedisTemplate();
         redisTemplate.setConnectionFactory(jedisConnectionFactory());
-//        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+
+        RedisSerializer<String> stringRedisSerializer = new StringRedisSerializer();
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+        redisTemplate.setValueSerializer(stringRedisSerializer);
+        /*此处可以用fastjson替换jackson，fastjson序列化时null自动过滤，需要额外设置。*/
+        /*FastJsonRedisSerializer fastJsonRedisSerializer = new FastJsonRedisSerializer(Object.class);*/
+        /*设置fastjson序列化时不过滤null值。*/
+       /* FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(SerializerFeature.WriteMapNullValue);
+        fastJsonRedisSerializer.setFastJsonConfig(fastJsonConfig);*/
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer(Object.class);
+        /*此处作用是将序列化的类地址放入redis，解析时需要同包同类名，因此不是很通用，这里先注释掉*/
 //        ObjectMapper om = new ObjectMapper();
 //        om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
 //        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
 //        jackson2JsonRedisSerializer.setObjectMapper(om);
-//        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
-//        redisTemplate.afterPropertiesSet();
+        redisTemplate.setValueSerializer(jackson2JsonRedisSerializer);
+        redisTemplate.afterPropertiesSet();
         return redisTemplate;
     }
 }

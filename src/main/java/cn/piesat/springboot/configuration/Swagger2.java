800e7d5a69c26bdf3efb474781040aaa08f6f0b5
506683059@qq.com <506683059@qq.com>
2018-09-11 17:01:53 +0800

    restfun拼写错误，更改为restful

--- src/main/java/cn/piesat/springboot/configuration/Swagger2.java
+++ src/main/java/cn/piesat/springboot/configuration/Swagger2.java
@@ -29,7 +29,7 @@ public class Swagger2 {
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
                 .title("api文档")
-                .description("简单优雅的restfun风格")
+                .description("简单优雅的restful风格")
                 .version("1.0")
                 .build();
     }
